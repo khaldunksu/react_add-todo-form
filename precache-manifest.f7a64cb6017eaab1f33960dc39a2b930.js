4df84d7d356447d1c9b70006ee36e48189b0d99a
ksukhaldun <kseniya.khaldun@gmail.com>
2019-12-11 22:13:24 +0300

    Updates

---
+++ precache-manifest.f7a64cb6017eaab1f33960dc39a2b930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dc2dd9860d3cff18985fb2800b8c22",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "d39c65b19c1e90f667b3",
+    "url": "/react_add-todo-form/static/css/main.cc483695.chunk.css"
+  },
+  {
+    "revision": "710e1266e890094367a4",
+    "url": "/react_add-todo-form/static/js/2.36d60507.chunk.js"
+  },
+  {
+    "revision": "d39c65b19c1e90f667b3",
+    "url": "/react_add-todo-form/static/js/main.27d28889.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  },
+  {
+    "revision": "977817b5aa4ffca4d0f5edcff12bc5e0",
+    "url": "/react_add-todo-form/static/media/background.977817b5.jpg"
+  }
+]);
